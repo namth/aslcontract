2ff78006fe9868c767ef367d7882c63f2267fd2a
Nam Tran <namth.pass@gmail.com>
2024-11-20 16:39:56 +0700

    Add sidebar and homepage templates, enhance CSS styles for buttons and icons

---
+++ sidebar.php
@@ -0,0 +1,52 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link" href="../../index.html">
+                <i class="mdi mdi-grid-large menu-icon"></i>
+                <span class="menu-title">Dashboard</span>
+            </a>
+        </li>
+        <li class="nav-item nav-category">Quản lý file</li>
+        <li class="nav-item">
+            <a class="nav-link" href="#ui-basic">
+                <i class="menu-icon mdi mdi-file-document-plus-outline"></i>
+                <span class="menu-title">Tạo hợp đồng</span>
+                <!-- <i class="menu-arrow"></i> -->
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#ui-basic">
+                <i class="menu-icon mdi mdi-file-table-outline"></i>
+                <span class="menu-title">Tạo mẫu hợp đồng</span>
+                <!-- <i class="menu-arrow"></i> -->
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#ui-basic">
+                <i class="menu-icon mdi mdi-folder-table-outline"></i>
+                <span class="menu-title">Danh mục</span>
+                <!-- <i class="menu-arrow"></i> -->
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#ui-basic">
+                <i class="menu-icon mdi mdi-layers-outline"></i>
+                <span class="menu-title">Data Source</span>
+                <!-- <i class="menu-arrow"></i> -->
+            </a>
+        </li>
+        <li class="nav-item nav-category">Cài đặt</li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <i class="menu-icon mdi mdi-account-circle-outline"></i>
+                <span class="menu-title">Tài khoản</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">
+                <i class="menu-icon mdi mdi-file-document"></i>
+                <span class="menu-title">Documentation</span>
+            </a>
+        </li>
+    </ul>
+</nav>
